f56ad8fbbc204d93e3e4d19dce880f356bea59ee
Danish <danish@pointlabs.org>
2022-09-16 18:32:42 +0500

    PZ-467 with loader added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,30 +7,41 @@ import TubeManager from "../services/TubeManager";
 import "../../node_modules/video-react/dist/video-react.css"; // import css
 import "../assets/styles/Home.css";
 import point from "../services/PointSDK";
+import { InfinitySpin } from "react-loader-spinner";
 
 export default function Home() {
   const [videos, setVideos] = useState([]);
   const [totalVideosLength, setTotalVideosLength] = useState(0);
   const [totalPageCounter, setTotalPageCounter] = useState(1);
   const [currentPage, setCurrentPage] = useState(1);
+  const [loader, setLoader] = useState(false);
+
   let numberOfPagination = 20;
 
   const getVideos = async () => {
     try {
+      setLoader(true);
       TubeManager.getAllVideosLength().then(function (_length) {
         setTotalVideosLength(_length);
-        setTotalPageCounter(_length > numberOfPagination ? Math.ceil(_length/numberOfPagination) : 1);
+        setTotalPageCounter(
+          _length > numberOfPagination
+            ? Math.ceil(_length / numberOfPagination)
+            : 1
+        );
         setCurrentPage(1);
-        TubeManager.getPaginatedVideos(currentPage, numberOfPagination).then(function (_res) {
-          console.log('_res',_res)
-          let tempVids = [];
-          for(var i=0;i<_res.length;i++){
-            if(_res[i][0] != "0"){
-              tempVids.push(_res[i]);
+        TubeManager.getPaginatedVideos(currentPage, numberOfPagination).then(
+          function (_res) {
+            console.log("_res", _res);
+            let tempVids = [];
+            for (var i = 0; i < _res.length; i++) {
+              if (_res[i][0] != "0") {
+                tempVids.push(_res[i]);
+              }
             }
+            setVideos(tempVids);
+            setLoader(false);
           }
-          setVideos(tempVids);
-        });
+        );
       });
     } catch (error) {
       console.log(error);
@@ -47,24 +58,28 @@ export default function Home() {
     items.push(
       <Pagination.Item key={number} active={number === currentPage}>
         {number}
-      </Pagination.Item>,
+      </Pagination.Item>
     );
   }
 
   const paginateHandle = (event) => {
     var itemClicked = event.target.text;
-    if(itemClicked !== undefined){
+    if (itemClicked !== undefined) {
+      setLoader(true);
       setCurrentPage(parseInt(itemClicked));
-      let cursor = itemClicked > 1 ? ((itemClicked-1)*numberOfPagination) : 1;
-      console.log('cursor',cursor)
-      TubeManager.getPaginatedVideos(cursor, numberOfPagination).then(function (_res) {
+      let cursor = itemClicked > 1 ? (itemClicked - 1) * numberOfPagination : 1;
+      console.log("cursor", cursor);
+      TubeManager.getPaginatedVideos(cursor, numberOfPagination).then(function (
+        _res
+      ) {
         let tempVids = [];
-        for(var i=0;i<_res.length;i++){
-          if(_res[i][0] != "0"){
+        for (var i = 0; i < _res.length; i++) {
+          if (_res[i][0] != "0") {
             tempVids.push(_res[i]);
           }
         }
         setVideos(tempVids);
+        setLoader(false);
       });
     }
   };
@@ -77,43 +92,56 @@ export default function Home() {
             <h2>Home</h2>
           </Row>
           <Row>
-            {videos.length > 0 &&
-              videos.map((_item) => {
-                return (
-                  <>
-                    <Col xl={3} lg={4} sm={6}>
-                      <div className="video-card">
-                        <Player
-                          // src={URL.createObjectURL(_item[2])}
-                          src={`/_storage/${_item[2]}`}
-                        >
-                          <BigPlayButton position="center" />
-                        </Player>
-                        <Link
-                          to={`/video-detail/` + _item[0]}
-                          className="video-name mb-0"
-                        >
-                          {_item[3]}
-                        </Link>
-                        <p className="video-author mb-0">
-                          <span>{_item[5]}</span>
-                          <br/>
-                          {_item[1].substring(0, 2) +
-                            " ... " +
-                            _item[1].substring(
-                              _item[1].length,
-                              _item[1].length - 3
-                            )}
-                        </p>
-                      </div>
-                    </Col>
-                  </>
-                );
-              })}
+            {loader ? (
+              <>
+                <InfinitySpin width="200" color="#4fa94d" />
+              </>
+            ) : (
+              <>
+                {videos.length > 0 &&
+                  videos.map((_item) => {
+                    return (
+                      <>
+                        <Col xl={3} lg={4} sm={6}>
+                          <div className="video-card">
+                            <Player
+                              // src={URL.createObjectURL(_item[2])}
+                              src={`/_storage/${_item[2]}`}
+                            >
+                              <BigPlayButton position="center" />
+                            </Player>
+                            <Link
+                              to={`/video-detail/` + _item[0]}
+                              className="video-name mb-0"
+                            >
+                              {_item[3]}
+                            </Link>
+                            <p className="video-author mb-0">
+                              <span>{_item[5]}</span>
+                              <br />
+                              {_item[1].substring(0, 2) +
+                                " ... " +
+                                _item[1].substring(
+                                  _item[1].length,
+                                  _item[1].length - 3
+                                )}
+                            </p>
+                          </div>
+                        </Col>
+                      </>
+                    );
+                  })}
+              </>
+            )}
           </Row>
-          <div className="pagination-wrapper">
-            <Pagination onClick={paginateHandle} size="sm">{items}</Pagination>
+          {!loader ? <>
+            <div className="pagination-wrapper">
+            <Pagination onClick={paginateHandle} size="sm">
+              {items}
+            </Pagination>
           </div>
+          </> : <></>}
+          
         </Container>
       </div>
     </>
--- src/pages/MyVideos.jsx
+++ src/pages/MyVideos.jsx
@@ -8,19 +8,21 @@ import { Link } from "wouter";
 import "../../node_modules/video-react/dist/video-react.css"; // import css
 import "../assets/styles/Home.css";
 import "react-toastify/dist/ReactToastify.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { InfinitySpin } from "react-loader-spinner";
 
 export default function MyVideos() {
   const [identityName, setIdentityName] = useState("");
   const [address, setAddress] = useState(undefined);
   const [videos, setVideos] = useState([]);
+  const [loader, setLoader] = useState(false);
 
   const getAccount = async () => {
     try {
       let wallet = await point.getIdentity();
-      setIdentityName(wallet['identity']);
-      setAddress(wallet['address']);
+      setIdentityName(wallet["identity"]);
+      setAddress(wallet["address"]);
     } catch (error) {
       toast.error(error.message, { position: "bottom-center" });
     } finally {
@@ -29,16 +31,25 @@ export default function MyVideos() {
 
   const getVideos = async () => {
     try {
-        TubeManager.getVideosByUser(address).then(function (_res) {
-          let tempVids = [];
-          for(var i=0;i<_res.length;i++){
-            if(_res[i][0] != "0"){
-              tempVids.push(_res[i]);
-            }
+      setLoader(true);
+
+      setTimeout(function() { 
+
+
+      TubeManager.getVideosByUser(address).then(function (_res) {
+        let tempVids = [];
+        for (var i = 0; i < _res.length; i++) {
+          if (_res[i][0] != "0") {
+            tempVids.push(_res[i]);
           }
-          console.log('tempVids',tempVids)
-          setVideos(tempVids);
-        });
+        }
+        console.log("tempVids", tempVids);
+        setVideos(tempVids);
+        setLoader(false);
+      });
+
+    }, 2000);
+
     } catch (error) {
       console.log(error);
     } finally {
@@ -63,43 +74,59 @@ export default function MyVideos() {
             <h2>My Videos</h2>
           </Row>
           <Row>
-            {videos.length > 0 &&
-              videos.map((_item) => {
-                return (
-                  <>
-                    <Col xl={3} lg={4} sm={6}>
-                      <div className="video-card">
-                      <Player 
-                        // src={URL.createObjectURL(_item[2])}
-                         src={`/_storage/${_item[2]}`}>
-                          <BigPlayButton position="center" />
-                        </Player>
-                        <div className="video-dd d-flex justify-content-between align-items-end">
-                          <div>
-                            <Link
-                            to={`/video-detail/`+_item[0]}
-                            className="video-name mb-0"
-                          >
-                            {_item[3]}
-                          </Link>
-                          <p className="video-author mb-0">
-                            <span>{_item[5]}</span>
-                            <br/>
-                            <span>{_item[1].substring(0, 2) +
-                              " ... " +
-                              _item[1].substring(
-                                _item[1].length,
-                                _item[1].length - 3
-                              )}</span>
-                          </p>
+            {loader ? (
+              <>
+                <InfinitySpin width="200" color="#4fa94d" />
+              </>
+            ) : (
+              <>
+                {videos.length > 0 &&
+                  videos.map((_item) => {
+                    return (
+                      <>
+                        <Col xl={3} lg={4} sm={6}>
+                          <div className="video-card">
+                            <Player
+                              // src={URL.createObjectURL(_item[2])}
+                              src={`/_storage/${_item[2]}`}
+                            >
+                              <BigPlayButton position="center" />
+                            </Player>
+                            <div className="video-dd d-flex justify-content-between align-items-end">
+                              <div>
+                                <Link
+                                  to={`/video-detail/` + _item[0]}
+                                  className="video-name mb-0"
+                                >
+                                  {_item[3]}
+                                </Link>
+                                <p className="video-author mb-0">
+                                  <span>{_item[5]}</span>
+                                  <br />
+                                  <span>
+                                    {_item[1].substring(0, 2) +
+                                      " ... " +
+                                      _item[1].substring(
+                                        _item[1].length,
+                                        _item[1].length - 3
+                                      )}
+                                  </span>
+                                </p>
+                              </div>
+                              <Link to={`/edit-video/` + _item[0]}>
+                                <FontAwesomeIcon
+                                  className="edit-video"
+                                  icon={faPenToSquare}
+                                />
+                              </Link>
+                            </div>
                           </div>
-                          <Link to={`/edit-video/`+_item[0]}><FontAwesomeIcon className="edit-video" icon={faPenToSquare} /></Link>
-                        </div>
-                      </div>
-                    </Col>
-                  </>
-                );
-              })}
+                        </Col>
+                      </>
+                    );
+                  })}
+              </>
+            )}
           </Row>
         </Container>
       </div>
